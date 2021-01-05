ef0fc9e63dca486929e2557a32d653b7f312cfdb
danx0r <dan.miller@eye0.com>
2021-01-04 17:31:00 -0800

    well is a start

--- fdiff.py
+++ fdiff.py
@@ -11,16 +11,21 @@ sr, w1 =  wf.read(fn1)
 sr, w2 =  wf.read(fn2)
 
 i = 0
-out = []
-while i < len(wav)-WINDOW:
-    dft = np.fft.fft(wav[i: i+WINDOW])
-    if i > len(wav)//2:
-        for j in range(WINDOW//32, WINDOW-WINDOW//32-1):
-            dft[j] = 0
-    out.append( np.int16(np.real(np.fft.ifft(dft))) )
+tot = min(len(w1), len(w2))
+cnt = tot // WINDOW
+errs = []
+while i < tot - WINDOW:
+    d1 = np.fft.fft(w1[i: i + WINDOW])
+    d1 = np.absolute(d1)
+    d2 = np.fft.fft(w2[i: i + WINDOW])
+    d2 = np.absolute(d2)
+    d = d1-d2
+    d2 = d * d
+    avg = np.average(d2)
+    rmse = avg ** .5
+    errs.append(rmse)
     i += WINDOW
 
-wo = np.concatenate(out)
+err = sum(errs) / len(errs)
 
-print (len(wo))
-wf.write("out.wav", sr, wo)
+print ("ERR:", err)
