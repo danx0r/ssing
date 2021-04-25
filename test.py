518d6a603bd890d5960a32e067235e5e64119cf4
danx0r <dan.miller@eye0.com>
2021-04-25 16:19:03 -0700

    somewhere

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os, sys, time, math
-# import numpy as np
+import numpy as np
 from scipy.io import wavfile as wf
 import matplotlib.pyplot as plt
 pi2 = math.pi*2
@@ -9,8 +9,19 @@ sec=1
 chunks=3
 freq=3.4
 
+def plot_complex(a):
+    mag = []
+    phase = []
+    for x in a:
+        mag.append(abs(x))
+        p = np.angle(x) % pi2
+        phase.append(p)
+    plt.plot(mag)
+    plt.plot(phase)
+
 # wo = np.zeros(sr*sec, np.int16)
 wo = []
+dfts=[]
 phase = 0
 for chunk in range(chunks):
     for i in range(sr*sec):
@@ -22,6 +33,12 @@ for chunk in range(chunks):
     t = i / sr
     phase = (phase + t * freq * pi2) % pi2
     print (phase)
+    j = len(wo)
+    dft = np.fft.fft(wo[j-sr: j])
+    dfts.append(dft)
 
 plt.plot(wo)
 plt.show()
+for dft in dfts:
+    plot_complex(dft)
+    plt.show()
