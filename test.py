f0c0d22321e4852fd915c6e4eeaf700a38df9640
danx0r <dan.miller@eye0.com>
2021-04-25 15:54:29 -0700

    work like a charme

--- test.py
+++ test.py
@@ -2,24 +2,26 @@ import os, sys, time, math
 # import numpy as np
 from scipy.io import wavfile as wf
 import matplotlib.pyplot as plt
+pi2 = math.pi*2
 
 sr=40
 sec=1
-chunks=2
-freq=3
+chunks=3
+freq=3.4
 
 # wo = np.zeros(sr*sec, np.int16)
 wo = []
+phase = 0
 for chunk in range(chunks):
     for i in range(sr*sec):
-        # s = i % 256 * 16
         t = i/sr
-        s = math.sin(t*freq*math.pi*2)
+        yy = phase + t * freq * pi2
+        s = math.sin(yy)
         wo.append(s)
-
-    # wf.write("out.wav", sr, wo)
-
-# print(wo)
+    i += 1
+    t = i / sr
+    phase = (phase + t * freq * pi2) % pi2
+    print (phase)
 
 plt.plot(wo)
 plt.show()
