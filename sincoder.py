a33045c7ffaa7ac97af1ef98e5eba738c928c75d
danx0r <dan.miller@eye0.com>
2021-05-02 16:48:11 -0700

    brutish force

--- sincoder.py
+++ sincoder.py
@@ -6,7 +6,7 @@ SAMP_SEC = 100
 
 def sindata(freq, phase, seconds):
     data = []
-    for i in range(SAMP_SEC * seconds):
+    for i in range(int(SAMP_SEC * seconds)):
         y = math.sin(phase)
         data.append(y)
         phase += PI2 * freq / SAMP_SEC
@@ -18,6 +18,46 @@ def muldata (a, b):
         data.append(x * y)
     return data
 
+def mulscalar (a, b):
+    data = []
+    for x in a:
+        data.append(x * b)
+    return data
+
+def subdata (a, b):
+    data = []
+    for x, y in zip(a, b):
+        data.append(y - x)
+    return data
+
+def energy(a):
+    sum = 0
+    for x in a:
+        sum += x ** 2
+    return sum ** .5
+
+#detect freq, phase, amplitude of largest component
+def max_fpa(data):
+    sec = len(data) / SAMP_SEC
+    err = energy(data)
+    print ("FPA initial err:", err)
+    for p in [x * PI2/16 for x in range(1)]:
+        for f in [x / 10 for x in range(10, 41)]:
+            basis = sindata(f, p, sec)
+            for a in [1]: #[x / 4 for x in range(1, 11)]:
+                b = mulscalar(basis, a)
+                c = subdata(data, b)
+                e2 = energy(c)
+                print ("FPA test freq:", f, "err:", e2, "new:", e2<err)
+                if e2 < err:
+                    err = e2
+                    minf = f
+                    minp = p
+                    mina = a
+    return minf, minp, mina
+
+
+"""
 sin = sindata(2, 0, 1)
 # print (sin)
 
@@ -28,3 +68,13 @@ mul = muldata(sin, cos)
 
 plt.plot(list(zip(sin, cos, mul)), marker = '.')
 plt.show()
+"""
+
+test = sindata(3, 0, 1)
+
+f, p, a = max_fpa(test)
+print (f, p, a)
+
+plt.plot(test, marker = '.')
+plt.show()
+
