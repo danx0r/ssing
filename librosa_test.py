cbaaa60f8a65f57814e26cd70001047caced4273
danx0r <dan.miller@eye0.com>
2021-05-10 12:53:18 -0700

    Except 4 the few we take home to experiment

--- librosa_test.py
+++ librosa_test.py
@@ -1,9 +1,14 @@
+import sys
+from librosa import griffinlim
 from librosa.feature import melspectrogram as mel
 from librosa.feature.inverse import mel_to_audio as imel
+from librosa.feature.inverse import mel_to_stft as m2stft
 from librosa import load
 import soundfile as sf
 
-aud=load("old/mel4.wav", sr=44100)[0]
+aud=load(sys.argv[1], sr=44100)[0]
 m=mel(aud,sr=44100, hop_length=256, n_fft=1024)
-rec=imel(m, sr=44100, hop_length=256, n_fft=1024)
+# rec=imel(m, sr=44100, hop_length=256, n_fft=1024)
+S = m2stft(m, sr=44100, n_fft=1024)
+rec = griffinlim(S, hop_length=256)
 sf.write('mel_recon.wav', rec, 44100)
